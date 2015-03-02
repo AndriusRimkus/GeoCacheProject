17c36bb22e52b5290b8c9cdac88474d6a6cd8f84
FrostySoup <Giedrius3600@gmail.com>
2015-03-02 12:33:22 +0200

    Šiektiek su layout padirbėjau

--- app/src/main/java/com/geocache/nav/geocacheproject/BarcodeScanner.java
+++ app/src/main/java/com/geocache/nav/geocacheproject/BarcodeScanner.java
@@ -1,7 +1,16 @@
 package com.geocache.nav.geocacheproject;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Giedrius on 2015.02.28.
  */
-public class BarcodeScanner {
+public class BarcodeScanner extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scanner_scan);
+    }
 }
