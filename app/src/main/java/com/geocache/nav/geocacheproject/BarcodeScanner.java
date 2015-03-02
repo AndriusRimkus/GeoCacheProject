d1a7e0c97b3d832b4a98ca70fc3fd5e6393288ad
FrostySoup <Giedrius3600@gmail.com>
2015-03-02 15:17:30 +0200

    Jau gali nuskenuoti barkodus (veikia tik ant realaus prietaiso)

--- app/src/main/java/com/geocache/nav/geocacheproject/BarcodeScanner.java
+++ app/src/main/java/com/geocache/nav/geocacheproject/BarcodeScanner.java
@@ -2,15 +2,52 @@ package com.geocache.nav.geocacheproject;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 /**
  * Created by Giedrius on 2015.02.28.
  */
 public class BarcodeScanner extends Activity {
 
+    private Button scanBtn;
+    private TextView formatTxt, contentTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scanner_scan);
+        scanBtn = (Button)findViewById(R.id.scan_button);
+        formatTxt = (TextView)findViewById(R.id.scan_format);
+        contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn.setOnClickListener(skenuotiPaspausta);
     }
+
+    View.OnClickListener skenuotiPaspausta = new View.OnClickListener() {
+        public void onClick(View v) {
+            IntentIntegrator scanIntegrator = new IntentIntegrator(BarcodeScanner.this);
+            scanIntegrator.initiateScan();
+        }
+    };
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanningResult != null) {
+            String scanContent = scanningResult.getContents();
+            String scanFormat = scanningResult.getFormatName();
+            formatTxt.setText("FORMAT: " + scanFormat);
+            contentTxt.setText("CONTENT: " + scanContent);
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "No scan data received!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
 }
